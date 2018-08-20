e7754606928001bf554a2904bef404f930639794
Allen <allenstedman@gmail.com>
2018-08-20 15:45:49 -0700

    tabbed format

--- heroku/main.py
+++ heroku/main.py
@@ -25,39 +25,83 @@ from bokeh.palettes import Spectral6
 train = pd.read_csv('heroku/data/combined_train.csv')
 test = pd.read_csv('heroku/data/combined_test.csv')
 #spray = pd.read_csv('assets/input/spray.csv')
+x2007 = datetime(list(train[train['Year'] == '2007']['Date']))
+y2007 = list(train[train['Year'] == '2007']['Tavg'])
+x2009 = datetime(list(train[train['Year'] == '2009']['Date']))
+y2009 = list(train[train['Year'] == '2009']['Tavg'])
+x2011 = datetime(list(train[train['Year'] == '2011']['Date']))
+y2011 = list(train[train['Year'] == '2011']['Tavg'])
+x2013 = datetime(list(train[train['Year'] == '2013']['Date']))
+y2013 = list(train[train['Year'] == '2013']['Tavg'])
+x2015 = datetime(list(train[train['Year'] == '2015']['Date']))
+y2015 = list(train[train['Year'] == '2015']['Tavg'])
 
-x_var = ['Negative', 'Positive'] #train['WnvPresent'].value_counts(normalize = False).index
-counts = train['WnvPresent'].value_counts(normalize = False)
-source = ColumnDataSource(data = dict(labels = x_var, counts = counts))
+data3 = {'temperature2007': y2007,
+        'date2007': x2007,
+         'range2007': list(range(len(x2007)))
+       }
+data4 = {
+        'temperature2009': y2009,
+        'date2009': x2009
+}
+data5 = {
+        'temperature2011': y2011,
+        'date2011': x2011    
+}
+data6 = {
+        'temperature2013': y2013,
+        'date2013': x2013   
+}
+source3 = ColumnDataSource(data = data3)
+source4 = ColumnDataSource(data = data4)
+source5 = ColumnDataSource(data = data5)
+source6 = ColumnDataSource(data = data6)
 
-
-
-p1 = figure(x_range = x_var, title = 'West Nile Class Balance',
-            plot_height = 400, plot_width = 400, tools = 'save')
-
-p1.vbar(source = source,
-        x = 'labels',
-        top = 'counts',
-        bottom = 0,
-        width = .9,
-        #fill_color ="green",
-        line_color ="white",
-        line_width = 2,
-        fill_color=factor_cmap('labels', palette = Spectral6, factors = x_var)
-       )
-
-p1.title.align = 'center'
-p1.add_tools(HoverTool(
+p3 = figure(title = 'Average Temperature (2007)', x_axis_type="datetime", plot_height = 600, plot_width = 800)
+p3.circle('date2007' ,'temperature2007', source = source3)
+p3.title.align = 'center'
+p3.title.text_font_size = '16pt'
+p3.add_tools(HoverTool(
     tooltips=[
-        ("Label", "$index"),
-     ("Total", "@counts")
+        ("Date", "@date2007"),
+     ("Temp", "@temperature2007")
     ]))
 
-p2 = figure(title = "Average Temperature Aggregate", tools = 'save',
-           background_fill_color = '#deebf7')
+p4 = figure(title = 'Average Temperature (2009)', x_axis_type="datetime", plot_height = 600, plot_width = 800)
+p4.circle('date2009' ,'temperature2009', source = source4)
+p4.title.align = 'center'
+p4.title.text_font_size = '16pt'
+p4.add_tools(HoverTool(
+    tooltips=[
+        ("Date", "@date2009"),
+     ("Temp", "@temperature2009")
+    ]))
 
-hist, edges = np.histogram(train['Tavg'], density = False, bins = 20)
+p5 = figure(title = 'Average Temperature (2011)', x_axis_type="datetime", plot_height = 600, plot_width = 800)
+p5.circle('date2011' ,'temperature2011', source = source5)
+p5.title.align = 'center'
+p5.title.text_font_size = '16pt'
+p5.add_tools(HoverTool(
+    tooltips=[
+        ("Date", "@date2011"),
+     ("Temp", "@temperature2011")
+    ]))
 
-p2.quad(top = hist, bottom = 0, left = edges[:-1], right =edges[1:], fill_color = '#6baed6', line_color = '#084594')
+p6 = figure(title = 'Average Temperature (2013)', x_axis_type="datetime", plot_height = 600, plot_width = 800)
+p6.circle('date2013' ,'temperature2013', source = source6)
+p6.title.align = 'center'
+p6.title.text_font_size = '16pt'
+p6.add_tools(HoverTool(
+    tooltips=[
+        ("Date", "@date2013"),
+     ("Temp", "@temperature2013")
+    ]))
 
-curdoc().add_root(column(p1, p2))
+tab1 = Panel(child = p3, title = 'Ave Temp vs Time (2007)')
+tab2 = Panel(child = p4, title = 'Ave Temp vs Time (2009)')
+tab3 = Panel(child = p5, title = 'Ave Temp vs Time (2011)')
+tab4 = Panel(child = p6, title = 'Ave Temp vs Time (2013)')
+layout = Tabs(tabs=[tab1, tab2, tab3, tab4])
+#show(layout)
+
+curdoc().add_root(layout)
